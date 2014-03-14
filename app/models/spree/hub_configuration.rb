c8aeeaeb1193bec6188625eb2c4ea5a3336996d4
Peter Berkenbosch <peter@pero-ict.nl>
2014-03-14 10:23:09 +0100

    hmac is optional

--- app/models/spree/hub_configuration.rb
+++ app/models/spree/hub_configuration.rb
@@ -4,5 +4,6 @@ module Spree
     preference :hub_store_id, :string
     preference :hub_token, :string
     preference :hub_push_uri, :string, :default => "https://push.hubapp.io"
+    preference :use_hmac, :boolean, :default => true
   end
 end
--- lib/spree/hub/client.rb
+++ lib/spree/hub/client.rb
@@ -10,13 +10,16 @@ module Spree
 
         uri = Spree::Hub::Config[:hub_push_uri]
         store_id = Spree::Hub::Config[:hub_store_id]
-        token = Spree::Hub::Config[:hub_token]
-
-        timestamp  = Time.now.utc.to_i
+        access_token = Spree::Hub::Config[:hub_token]
+        use_hmac = Spree::Hub::Config[:use_hmac]
         json_payload = JSON.generate(payload)
-        data = "#{json_payload}#{timestamp}"
-        sha1 = OpenSSL::Digest.new('sha1')
-        access_token = OpenSSL::HMAC.hexdigest(sha1, token, data)
+
+        if use_hmac
+          timestamp  = Time.now.utc.to_i
+          data = "#{json_payload}#{timestamp}"
+          sha1 = OpenSSL::Digest.new('sha1')
+          access_token = OpenSSL::HMAC.hexdigest(sha1, token, data)
+        end
 
         HTTParty.post(
           uri,
