e83afdb58a6e57b8099b3f22ae50c300e2f0f1fa
Peter Berkenbosch <peter@pero-ict.nl>
2014-03-14 11:24:46 +0100

    always send the timestamp the payload param is in json.

--- lib/spree/hub/client.rb
+++ lib/spree/hub/client.rb
@@ -6,16 +6,15 @@ module Spree
   module Hub
     class Client
 
-      def self.push(payload)
+      def self.push(json_payload)
 
         uri = Spree::Hub::Config[:hub_push_uri]
         store_id = Spree::Hub::Config[:hub_store_id]
         access_token = Spree::Hub::Config[:hub_token]
         use_hmac = Spree::Hub::Config[:use_hmac]
-        json_payload = JSON.generate(payload)
+        timestamp  = Time.now.utc.to_i
 
         if use_hmac
-          timestamp  = Time.now.utc.to_i
           data = "#{json_payload}#{timestamp}"
           sha1 = OpenSSL::Digest.new('sha1')
           access_token = OpenSSL::HMAC.hexdigest(sha1, token, data)
