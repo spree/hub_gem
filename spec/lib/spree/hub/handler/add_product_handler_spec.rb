fe95f3c55ff239fb73e90e7b0d24afca44c335ae
Peter Berkenbosch <peter@pero-ict.nl>
2014-03-18 13:02:36 +0000

    refacter the specs to be better readable.

--- spec/lib/spree/hub/handler/add_product_handler_spec.rb
+++ spec/lib/spree/hub/handler/add_product_handler_spec.rb
@@ -15,18 +15,34 @@ module Spree
 
           let(:handler) { Handler::AddProductHandler.new(message.to_json) }
 
-          it "imports a new product with variant master in the storefront" do
-            expect{handler.process}.to change{Spree::Variant.count}.by(1)
+          it "imports a new product in the storefront" do
             expect{handler.process}.to change{Spree::Product.count}.by(1)
           end
 
-          it "returns a Hub::Responder" do
-            responder = handler.process
-            expect(responder.class.name).to eql "Spree::Hub::Responder"
-            expect(responder.request_id).to eql message["request_id"]
-            expect(responder.code).to eql 200
-            expect(responder.summary).to match /Product \(\d\) and master variant \(\d\) are added/
+          it "imports a new variant master in the storefront" do
+            expect{handler.process}.to change{Spree::Variant.count}.by(1)
           end
+
+          context "response" do
+            let(:responder) { handler.process }
+
+            it "is a Hub::Responder" do
+              expect(responder.class.name).to eql "Spree::Hub::Responder"
+            end
+
+            it "returns the original request_id" do
+              expect(responder.request_id).to eql message["request_id"]
+            end
+
+            it "returns http 200" do
+              expect(responder.code).to eql 200
+            end
+
+            it "returns a summary with the created product and variant id's" do
+              expect(responder.summary).to match /Product \(\d\) and master variant \(\d\) are added/
+            end
+          end
+
         end
       end
 
