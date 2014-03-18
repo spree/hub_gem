6280fb9f6483334405f70049ec4fd047794bec83
Peter Berkenbosch <peter@pero-ict.nl>
2014-03-18 15:27:12 +0000

    add pry-debugger add taxon creation for products including specs

--- spec/lib/spree/hub/handler/add_product_handler_spec.rb
+++ spec/lib/spree/hub/handler/add_product_handler_spec.rb
@@ -34,6 +34,64 @@ module Spree
             end
           end
 
+          context "with taxons" do
+
+            context "and no taxons present in Spree" do
+              it "will create the taxonomies based on the first item" do
+                expect{handler.process}.to change{Spree::Taxonomy.count}.by(2)
+                expect(Spree::Taxonomy.find_by_name("Categories")).not_to be_nil
+                expect(Spree::Taxonomy.find_by_name("Brands")).not_to be_nil
+              end
+
+              it "will create the taxons recursively" do
+                expect{handler.process}.to change{Spree::Taxon.count}.by(6)
+              end
+            end
+
+            context "and some taxons already present in Spree" do
+              let!(:taxonomy) {create(:taxonomy, name: "Categories")}
+              it "will not create the already existing taxonomy" do
+                expect{handler.process}.to change{Spree::Taxonomy.count}.by(1)
+              end
+              it "will not create the root taxon" do
+                expect{handler.process}.to change{Spree::Taxon.count}.by(5)
+              end
+            end
+
+            it "will nest all properly" do
+              handler.process
+
+              # [
+              #   ["Categories", "Clothes", "T-Shrits"],
+              #   ["Brands", "Spree"],
+              #   ["Brands", "Open Source"]
+              # ]
+
+              categories = Spree::Taxonomy.find_by_name("Categories")
+              brands = Spree::Taxonomy.find_by_name("Brands")
+
+              # just "Clothes"
+              expect(categories.root.children.count).to eql 1
+
+              # ["Clothes", "T-Shrits"]
+              expect(categories.root.descendants.count).to eql 2
+
+              # just "T-Shrits"
+              expect(categories.root.leaves.count).to eql 1
+
+              # "Spree" and "Open Source"
+              expect(brands.root.children.count).to eql 2
+
+              # "Spree" and "Open Source"
+              expect(brands.root.descendants.count).to eql 2
+
+              # "Spree" and "Open Source"
+              expect(brands.root.leaves.count).to eql 2
+
+            end
+
+          end
+
           context "response" do
             let(:responder) { handler.process }
 
