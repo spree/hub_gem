fff63823d18374ce86691435884977a94adaa4a9
Peter Berkenbosch <peter@pero-ict.nl>
2014-03-18 15:27:12 +0000

    add pry-debugger add taxon creation for products including specs

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ gem 'spree', github: 'peterberkenbosch/spree', branch: "hub-client"
 
 gem 'hub_samples', :github => "spree/hub_samples", :group => :test
 gem "pry", :group => :test
+gem "pry-debugger", :group => :test
 
 gemspec
--- lib/spree/hub/handler/add_product_handler.rb
+++ lib/spree/hub/handler/add_product_handler.rb
@@ -3,6 +3,24 @@ module Spree
     module Handler
       class AddProductHandler < Base
 
+
+        def add_taxon(parent, taxon_names)
+          return parent if taxon_names.empty?
+          return if parent == nil
+
+          taxon_name = taxon_names.shift
+          # first_or_create is broken :(
+          taxon = Spree::Taxon.where(name: taxon_name, parent_id: parent.id).first
+          if taxon
+            parent.children << taxon
+          else
+            taxon = parent.children.create(name: taxon_name)
+          end
+          parent.save
+          add_taxon(taxon, taxon_names)
+        end
+
+
         def process
           params = @payload[:product]
 
@@ -12,10 +30,22 @@ module Spree
           taxons = params.delete(:taxons)
           option_types_params = params.delete(:options)
           images = params.delete(:images)
+          shipping_category_name = params.delete(:shipping_category)
 
           params[:slug] = permalink if permalink.present?
 
-          if shipping_category_name = params.delete(:shipping_category)
+          # taxons are stored as breadcrumbs in an nested array.
+          # [["Categories", "Clothes", "T-Shrits"], ["Brands", "Spree"], ["Brands", "Open Source"]]
+          if taxons
+            taxons.each do |taxons_path|
+              taxonomy_name = taxons_path.shift
+              taxonomy = Spree::Taxonomy.where(name: taxonomy_name).first_or_create
+              root_taxon = taxonomy.root
+              add_taxon(root_taxon, taxons_path)
+            end
+          end
+
+          if shipping_category_name
             shipping_category_id = Spree::ShippingCategory.where(name: shipping_category_name).first_or_create.id
             params[:shipping_category_id] = shipping_category_id
           end
