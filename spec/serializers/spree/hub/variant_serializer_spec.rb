b0878ecc960152dd402a6caac9026a3fbc6164fc
Peter Berkenbosch <peter@pero-ict.nl>
2014-04-03 14:57:57 +0200

    remove the nested product serializer and move back to match the documented JSON
    Fixes #10, #15

--- spec/serializers/spree/hub/variant_serializer_spec.rb
+++ spec/serializers/spree/hub/variant_serializer_spec.rb
@@ -17,21 +17,66 @@ module Spree
           expect(serialized_variant["cost_price"].class).to eql Float
         end
 
-        context "without options" do
+        it "serializes the available_on in ISO format" do
+          expect(serialized_variant["available_on"]).to match /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z/
+        end
 
+        it "serializes the slug as permalink" do
+          expect(serialized_variant["permalink"]).to eql variant.slug
+        end
+
+        it "serializes the shipping category name as shipping_category" do
+          expect(serialized_variant["shipping_category"]).to eql variant.shipping_category.name
+        end
+
+        context "without taxons" do
+
+          it "returns [] for 'taxons'" do
+            expect(serialized_variant["taxons"]).to eql []
+          end
+        end
+
+        context "taxons" do
+          let(:taxon)    { create(:taxon, name: 't-shirts', :parent => create(:taxon, name: 'Categories')) }
+          let(:taxon2)   { create(:taxon, name: 'modern') }
+          let(:product)  { variant.product }
+
+          before do
+            product.stub :taxons => [taxon, taxon2]
+          end
+
+          it "serailizes the taxons as nested arrays" do
+            expect(serialized_variant["taxons"]).to eql [["Categories", "t-shirts"], ["modern"]]
+          end
+        end
+
+        context "without options" do
           let(:variant) { create(:product).master }
 
           it "returns {} for 'options'" do
             expect(serialized_variant["options"].class).to eql Hash
             expect(serialized_variant["options"]).to be_empty
           end
-
         end
 
         context "options" do
           it "returns a hash with 'option_type => value'" do
             options_hash = {"Size" => "S"}
             expect(serialized_variant["options"]).to eql options_hash
+          end
+        end
+
+        context "for 'variant'" do
+          it "serializes the 'master' id as parent_id" do
+            expect(serialized_variant["parent_id"]).to eql variant.product.master.id
+          end
+        end
+
+        context "for master product" do
+          let(:variant) { create(:master_variant) }
+
+          it "serializes the parent_id as nil" do
+            expect(serialized_variant["parent_id"]).to eql nil
           end
         end
 
