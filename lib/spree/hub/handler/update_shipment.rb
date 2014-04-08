b5d0e72478ec35ede328c35ae3766c45109a5358
Sebastian Bean <sebastian@ginlanemedia.com>
2014-04-08 15:44:50 -0400

    chipments

--- lib/spree/hub/handler/update_shipment.rb
+++ lib/spree/hub/handler/update_shipment.rb
@@ -4,10 +4,13 @@ module Spree
       class UpdateShipmentHandler < Base
         def process
           current_order = Spree::Order.where(id: @payload[:order][:id]).first
-          shipment = current_order.shipments.first
 
-          shipment.tracking = @payload[:order][:tracking]
-          shipment.save
+          shipment = current_order.shipments.first
+          if shipment.present?
+            shipment.tracking = @payload[:order][:tracking]
+            shipment.state = "shipped"
+            shipment.save
+          end
           response "Order with shipments #{@payload[:order][:id]} was not found", 500 unless shipment
         end        
       end      
