7b2e640f78a4d353f858c9c39985e171cedc8b99
Peter Berkenbosch <peter@pero-ict.nl>
2014-04-10 14:45:46 +0200

    wip update_order

--- lib/spree/hub/handler/update_order_handler.rb
+++ lib/spree/hub/handler/update_order_handler.rb
@@ -4,10 +4,23 @@ module Spree
       class UpdateOrderHandler < OrderHandler
 
         def process
-          current_order = Spree::Order.where(number: @payload[:order][:id]).first
-          binding.pry
+          find_order(@payload[:order][:id])
 
-          response "Order with number #{@payload[:order][:id]} was not found", 500 unless current_order
+          order_params = OrderHandler.order_params(@payload[:order])
+          order_params.delete(:number)
+          order_params.delete(:id)
+          order_params.delete(:totals)
+
+          payments_attr = order_params.delete(:payments_attributes)
+          adjustments_attr = order_params.delete(:adjustments_attributes)
+          line_items_attr = order_params.delete(:line_items_attributes)
+          @order.update_attributes(order_params)
+
+        end
+
+        private
+        def find_order(number)
+          @order = Spree::Order.lock(true).find_by!(number: number)
         end
 
       end
