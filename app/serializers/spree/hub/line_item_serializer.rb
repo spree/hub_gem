9e603f5686b41ce05215be283f91c95128ff2200
Sebastian Bean <sebastian@ginlanemedia.com>
2014-04-15 15:31:58 -0400

    needs id attr, use utc iso dates

--- app/serializers/spree/hub/line_item_serializer.rb
+++ app/serializers/spree/hub/line_item_serializer.rb
@@ -3,7 +3,7 @@ require 'active_model/serializer'
 module Spree
   module Hub
     class LineItemSerializer < ActiveModel::Serializer
-      attributes :product_id, :name, :quantity, :price
+      attributes :id, :product_id, :name, :quantity, :price, :created_at, :updated_at
 
       has_one :variant, serializer: Spree::Hub::VariantSerializer
 
@@ -13,6 +13,14 @@ module Spree
 
       def price
         object.price.to_f
+      end
+
+      def created_at
+        object.created_at.getutc.iso8601
+      end
+
+      def updated_at
+        object.updated_at.getutc.iso8601
       end
 
     end
