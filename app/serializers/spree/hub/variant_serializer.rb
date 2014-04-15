ebd82d05e6af346e3ad9e9d0630b1ccfa6ba6199
Sebastian Bean <sebastian@ginlanemedia.com>
2014-04-15 15:36:29 -0400

    updated variant serializer with jirafe data

--- app/serializers/spree/hub/variant_serializer.rb
+++ app/serializers/spree/hub/variant_serializer.rb
@@ -4,8 +4,8 @@ module Spree
   module Hub
     class VariantSerializer < ActiveModel::Serializer
 
-      attributes :sku, :price, :cost_price, :options, :product_id
-      has_many :images, serializer: Spree::Hub::ImageSerializer
+      attributes :id, :sku, :price, :cost_price, :options, :name
+      attributes :product, :images
 
       def price
         object.price.to_f
@@ -17,6 +17,19 @@ module Spree
 
       def options
         object.option_values.each_with_object({}) {|ov,h| h[ov.option_type.presentation]= ov.presentation}
+      end
+
+      def name
+        object.product.name
+      end
+
+      def product
+        hash = {}
+        hash[:created_at] = object.product.created_at.getutc.iso8601
+        hash[:updated_at] = object.product.updated_at.getutc.iso8601
+
+        hash[:taxons] = object.product.taxons.map{|t| t.slice(:id, :name, :taxonomy_id)}
+        hash
       end
 
     end
