7f461c7e46c7fdb8afcf713ab1b0652bdd87c46e
John Dyer <lbrapid@gmail.com>
2014-04-16 14:55:48 -0400

    OrderSerializer sets json root to 'carts' if order state is not complete

--- spec/serializers/spree/hub/order_serializer_spec.rb
+++ spec/serializers/spree/hub/order_serializer_spec.rb
@@ -47,11 +47,20 @@ module Spree
           Spree::Hub::Config[:enable_auto_push] = true
         end
 
-        it "serializes Order object and push it to the hub" do
-          expect(HTTParty).to receive(:post)
-          described_class.push_it order
+        context "when status is 'complete'" do
+          it "serializes Order object and pushes it to the hub with root orders" do
+            expect(HTTParty).to receive(:post)
+            described_class.push_it order
+          end
         end
 
+        context "when status is not 'complete'" do
+          it "serializes Order object and pushes it to the hub with root carts" do
+            order = create(:order)
+            expect(HTTParty).to receive(:post)
+            described_class.push_it order
+          end
+        end
       end
     end
   end
