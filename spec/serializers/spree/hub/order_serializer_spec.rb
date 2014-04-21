3a46e72e5b97d01432496fcd98125f3f7c507894
Washington Luiz <huoxito@gmail.com>
2014-04-21 20:06:55 -0300

    Make order[:adjustments] closer to what hub expects
    Before it would only link adjustments directly associated with the order
    
    Fixes #29
    
    Conflicts:
    	spec/serializers/spree/hub/order_serializer_spec.rb

--- spec/serializers/spree/hub/order_serializer_spec.rb
+++ spec/serializers/spree/hub/order_serializer_spec.rb
@@ -5,20 +5,22 @@ module Spree
     describe OrderSerializer do
 
       let!(:order) {create(:shipped_order)}
-      let(:serialized_order) { OrderSerializer.new(order, root: false).to_json }
+      let(:serialized_order) do
+        JSON.parse(OrderSerializer.new(order, root: false).to_json)
+      end
 
       context "format" do
 
         it "uses the order number for id" do
-          expect(JSON.parse(serialized_order)["id"]).to eql order.number
+          expect(serialized_order["id"]).to eql order.number
         end
 
         it "uses status for the state" do
-          expect(JSON.parse(serialized_order)["status"]).to eql order.state
+          expect(serialized_order["status"]).to eql order.state
         end
 
         it "set's the placed_on to completed_at date in ISO format" do
-          expect(JSON.parse(serialized_order)["placed_on"]).to match /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z/
+          expect(serialized_order["placed_on"]).to match /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z/
         end
 
         context "totals" do
@@ -35,7 +37,14 @@ module Spree
           end
 
           it "has all the amounts for the order" do
-            expect(JSON.parse(serialized_order)["totals"]).to eql totals
+            expect(serialized_order["totals"]).to eql totals
+          end
+        end
+
+        context "adjustments key" do
+          it "shipment matches order shipping total value" do
+            shipping_hash = serialized_order["adjustments"].select { |a| a["name"] == "shipping" }.first
+            expect(shipping_hash["value"]).to eq order.shipment_total.to_f
           end
         end
       end
