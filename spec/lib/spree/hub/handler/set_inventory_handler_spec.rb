3b102df5b935c8fb0fceb2f85bbe5901417a9f16
Peter Berkenbosch <peter@pero-ict.nl>
2014-05-02 11:59:29 +0200

    add set_inventory

--- spec/lib/spree/hub/handler/set_inventory_handler_spec.rb
+++ spec/lib/spree/hub/handler/set_inventory_handler_spec.rb
@@ -3,6 +3,23 @@ require 'spec_helper'
 module Spree
   module Hub
     describe Handler::SetInventoryHandler do
+
+      let(:message) {::Hub::Samples::Inventory.request}
+      let(:handler) { Handler::SetInventoryHandler.new(message.to_json) }
+
+      describe "process" do
+
+        context "with stock item present" do
+          let!(:variant) { create(:variant, :sku => 'SPREE-T-SHIRT') }
+
+          it "will set the inventory to the supplied amount" do
+            expect{handler.process}.to change{variant.reload.count_on_hand}.from(5).to(93)
+          end
+
+        end
+
+      end
+
     end
   end
 end
