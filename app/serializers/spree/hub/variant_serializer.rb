9429c1067ded2703a1e5e1e90ed977658fc9b03b
Peter Berkenbosch <peter@pero-ict.nl>
2014-05-06 13:38:08 +0200

    update the serializers to reflect the product HOF format with nested variants.

--- app/serializers/spree/hub/variant_serializer.rb
+++ app/serializers/spree/hub/variant_serializer.rb
@@ -4,15 +4,8 @@ module Spree
   module Hub
     class VariantSerializer < ActiveModel::Serializer
 
-      attributes :id, :parent_id, :name, :sku, :description, :price, :cost_price,
-                  :available_on, :permalink, :meta_description, :meta_keywords,
-                  :shipping_category, :taxons, :options, :images
-
+      attributes :sku, :price, :cost_price, :options
       has_many :images, serializer: Spree::Hub::ImageSerializer
-
-      def parent_id
-        object.is_master? ? nil : object.product.master.id
-      end
 
       def price
         object.price.to_f
@@ -20,22 +13,6 @@ module Spree
 
       def cost_price
         object.cost_price.to_f
-      end
-
-      def available_on
-        object.available_on.iso8601 if object.available_on
-      end
-
-      def permalink
-        object.slug
-      end
-
-      def shipping_category
-        object.shipping_category.name
-      end
-
-      def taxons
-        object.product.taxons.collect {|t| t.root.self_and_descendants.collect(&:name)}
       end
 
       def options
--- spec/serializers/spree/hub/variant_serializer_spec.rb
+++ spec/serializers/spree/hub/variant_serializer_spec.rb
@@ -17,71 +17,21 @@ module Spree
           expect(serialized_variant["cost_price"].class).to eql Float
         end
 
-        it "does not serialize the available_on if it is not available" do
-          variant.available_on = nil
-          expect(serialized_variant["available_on"]).to be_nil
-        end
-
-        it "serializes the available_on in ISO format" do
-          expect(serialized_variant["available_on"]).to match /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z/
-        end
-
-        it "serializes the slug as permalink" do
-          expect(serialized_variant["permalink"]).to eql variant.slug
-        end
-
-        it "serializes the shipping category name as shipping_category" do
-          expect(serialized_variant["shipping_category"]).to eql variant.shipping_category.name
-        end
-
-        context "without taxons" do
-
-          it "returns [] for 'taxons'" do
-            expect(serialized_variant["taxons"]).to eql []
-          end
-        end
-
-        context "taxons" do
-          let(:taxon)    { create(:taxon, name: 't-shirts', :parent => create(:taxon, name: 'Categories')) }
-          let(:taxon2)   { create(:taxon, name: 'modern') }
-          let(:product)  { variant.product }
-
-          before do
-            product.stub :taxons => [taxon, taxon2]
-          end
-
-          it "serailizes the taxons as nested arrays" do
-            expect(serialized_variant["taxons"]).to eql [["Categories", "t-shirts"], ["modern"]]
-          end
-        end
-
         context "without options" do
+
           let(:variant) { create(:product).master }
 
           it "returns {} for 'options'" do
             expect(serialized_variant["options"].class).to eql Hash
             expect(serialized_variant["options"]).to be_empty
           end
+
         end
 
         context "options" do
           it "returns a hash with 'option_type => value'" do
             options_hash = {"Size" => "S"}
             expect(serialized_variant["options"]).to eql options_hash
-          end
-        end
-
-        context "for 'variant'" do
-          it "serializes the 'master' id as parent_id" do
-            expect(serialized_variant["parent_id"]).to eql variant.product.master.id
-          end
-        end
-
-        context "for master product" do
-          let(:variant) { create(:master_variant) }
-
-          it "serializes the parent_id as nil" do
-            expect(serialized_variant["parent_id"]).to eql nil
           end
         end
 
@@ -104,7 +54,7 @@ module Spree
             expect(serialized_variant["images"].count).to be 3
             dimension_hash = {"height" => 490, "width" => 489}
             3.times.each_with_index do |i|
-              expect(serialized_variant["images"][i]["url"]).to match /http:\/\/myapp.dev\/spree\/products\/\d{1}\/original\/thinking-cat.jpg\?\d*/
+              expect(serialized_variant["images"][i]["url"]).to match /http:\/\/myapp.dev\/spree\/products\/\d*\/original\/thinking-cat.jpg\?\d*/
               expect(serialized_variant["images"][i]["position"]).to eql i
               expect(serialized_variant["images"][i]["title"]).to eql "variant image #{i}"
               expect(serialized_variant["images"][i]["type"]).to eql "original"
@@ -114,6 +64,7 @@ module Spree
         end
 
       end
+
     end
   end
 end
