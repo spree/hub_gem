856ba0bfef2d2b0b29fb519036204f7c010bb7dc
Peter Berkenbosch <peter@pero-ict.nl>
2014-05-07 12:37:48 +0200

    spec the disable auto_push as well..

--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -5,15 +5,31 @@ module Spree
 
     let!(:order) { create(:order) }
 
-    before do
-      Spree::Hub::Config[:enable_push] = true
-      Spree::Hub::Config[:enable_auto_push] = true
+    context "configured to auto push" do
+      before do
+        Spree::Hub::Config[:enable_push] = true
+        Spree::Hub::Config[:enable_auto_push] = true
+      end
+
+      it "pushes serialized JSON after saved" do
+        expect(order).to receive(:push_to_hub)
+        order.save!
+      end
     end
 
-    it "pushes serialized JSON after saved" do
-      expect(order).to receive(:push_to_hub)
-      order.save!
+    context "when not configured to auto push" do
+      before do
+        Spree::Hub::Config[:enable_push] = true
+        Spree::Hub::Config[:enable_auto_push] = false
+      end
+
+      it "will not pushes serialized JSON after saved" do
+        expect(order).to_not receive(:push_to_hub)
+        order.save!
+      end
+
     end
+
 
   end
 end
