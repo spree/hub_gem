bde5d6afc7b4380123654cef1e5f28bc070bf2bb
Peter Berkenbosch <peter@pero-ict.nl>
2014-05-07 14:38:02 +0200

    only push Orders by default

--- app/models/spree/product_decorator.rb
+++
@@ -1,3 +0,0 @@
-Spree::Product.send(:include, Spree::Hub::AutoPush)
-Spree::Product.hub_serializer = "Spree::Hub::ProductSerializer"
-Spree::Product.json_root_name = 'products'
--- app/models/spree/stock_item_decorator.rb
+++
@@ -1,3 +0,0 @@
-Spree::StockItem.send(:include, Spree::Hub::AutoPush)
-Spree::StockItem.hub_serializer = "Spree::Hub::StockItemSerializer"
-Spree::StockItem.json_root_name = 'inventory'
