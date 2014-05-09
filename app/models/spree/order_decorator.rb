988c699fec5d58dc6b817d58505e4ff17d0675bb
Brian Quinn <briandquinn@gmail.com>
2014-05-09 11:54:26 +0100

    Add .push_when method to allow filtering of when to push objects, updated auto_push to use it, and added basic criteria to order decorator
    Fixes #49

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,3 +1,4 @@
 Spree::Order.send(:include, Spree::Hub::AutoPush)
 Spree::Order.hub_serializer = "Spree::Hub::OrderSerializer"
 Spree::Order.json_root_name = 'orders'
+Spree::Order.push_when = -> order { order.complete? }
